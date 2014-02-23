eaa5e373dd733a86147ac70711ac24926c527981
lbl <frantic.li.bl@gmail.com>
2014-02-23 13:45:41 +0800

    Add one more service.

---
+++ GroupReply/src/main/java/com/lbl/groupreply/DeliveryService.java
@@ -0,0 +1,16 @@
+package com.lbl.groupreply;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DeliveryService extends Service {
+    public DeliveryService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

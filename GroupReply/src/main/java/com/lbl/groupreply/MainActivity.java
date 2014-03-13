b3bda3d742dca440a9e739922621e43364c2badf
LBL <frantic.li.bl@gmail.com>
2014-03-13 21:11:59 +0800

    1. Able to handle send failures. 2. Change the output info when send list is smaller then 20.

--- GroupReply/src/main/java/com/lbl/groupreply/MainActivity.java
+++ GroupReply/src/main/java/com/lbl/groupreply/MainActivity.java
@@ -24,7 +24,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
